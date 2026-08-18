27337cf646f8eb3c9d5f244778be3be4ad68066b
apexspace9-a11y <apexspace9@gmail.com>
2026-08-18 10:49:50 +0700

    fix: complete About glass imports

--- app/src/main/java/com/starry/greenstash/ui/screens/settings/composables/AboutScreen.kt
+++ app/src/main/java/com/starry/greenstash/ui/screens/settings/composables/AboutScreen.kt
@@ -19,7 +19,6 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.LargeTopAppBar
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
@@ -36,6 +35,7 @@ import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.starry.greenstash.BuildConfig
 import com.starry.greenstash.R
